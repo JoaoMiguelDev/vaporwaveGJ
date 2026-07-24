1f992495f81b4d12c4adc3d9d433e8dd2a7343c6
JoaoMiguelDev <jmsabarreto67@gmail.com>
2026-07-24 12:31:51 -0300

    Detonator template

---
+++ Scripts/Character/Sword.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Sword : Node2D
+{
+	[Export] private AnimationPlayer Animation;
+	// [Export] private Area2D Hitbox;
+	private bool CanSlash = true;
+	public override void _Process(double delta)
+	{
+		LookAt(GetGlobalMousePosition());
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+		if (@event.IsActionPressed("attack") && CanSlash)
+		{
+			Animation.Play("slash");
+			CanSlash = false;
+		}
+    }
+
+	public void _on_animation_player_animation_finished(StringName anim_name)
+	{
+		if(anim_name == "slash")
+		{
+			// CanSlash = true;
+			Animation.Play("return");
+		}
+		if(anim_name == "return")
+		{
+			CanSlash = true;
+		}
+	}
+
+}

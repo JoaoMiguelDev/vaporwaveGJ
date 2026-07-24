129210491819c20145b172b3429f18668edd5a86
JoaoMiguelDev <jmsabarreto67@gmail.com>
2026-07-24 14:04:47 -0300

    Some sprites and player

--- Scripts/Character/Detonator.cs
+++ Scripts/Character/Detonator.cs
@@ -3,8 +3,7 @@ using System;
 
 public partial class Detonator : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 100.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{		

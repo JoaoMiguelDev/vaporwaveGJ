3446630229be1d4d6f3f8f646a4eb424f7e150c5
JoaoMiguelDev <jmsabarreto67@gmail.com>
2026-07-24 15:54:44 -0300

    PLayer death logic implemented

--- Scripts/Character/Detonator.cs
+++ Scripts/Character/Detonator.cs
@@ -3,23 +3,104 @@ using System;
 
 public partial class Detonator : CharacterBody2D
 {
+	[Signal] public delegate void HealthChangedEventHandler(int current, int max);
+	[Signal] public delegate void DiedEventHandler();
+	[Export] private Timer CanTakeDamageTimer;
 	public const float Speed = 150.0f;
+	public const float DashSpeed = 400f;
+	public const float DashTime = 0.12f;
+	private bool CanDash = true;
+	private float DashTimer = 0f;
+	private Vector2 DashDir = Vector2.Zero;
+	private const float DashReloadTime = 2f;
+	private float DashReloadTimer = 0; 
+
+	//Health variables
+	private const int MaxHealth = 3;
+	private int _health = 3;
+	public int Health
+	{
+    	get => _health;
+    	private set
+    	{
+        	_health = Mathf.Clamp(value, 0, MaxHealth);
+        	EmitSignal(SignalName.HealthChanged, _health, MaxHealth);
+        	if (_health == 0)
+			{
+				EmitSignal(SignalName.Died);
+				Die();
+			} 
+    	}
+	}
+
+	private bool IsDead = false;
+	private bool CanTakeDamage = true;
 
 	public override void _PhysicsProcess(double delta)
-	{		
-		Vector2 velocity = Velocity;
-		Vector2 direction = Input.GetVector("left", "right", "up", "down");
+	{
+		if(IsDead)
+			return;
+			
+	    var deltaF = (float)delta;
+	    Vector2 direction = Input.GetVector("left", "right", "up", "down");
 
-		if (direction != Vector2.Zero)
-		{
-			velocity = direction.Normalized() * Speed;				
-		}
-		else
-		{
-			velocity = Vector2.Zero;
-		}
+	    if (DashTimer > 0f)
+	    {
+	        DashTimer -= deltaF;
+	        Velocity = DashDir * DashSpeed;
+	    }
+	    else
+	    {
+	        if (!CanDash)
+	        {
+	            DashReloadTimer -= deltaF;
+	            if (DashReloadTimer <= 0f)
+	            {
+	                CanDash = true;
+	            }
+	        }
 
-		Velocity = velocity;
-		MoveAndSlide();
+	        if (CanDash && Input.IsActionJustPressed("dash") && direction != Vector2.Zero)
+	        {
+				GD.Print("Tô dando dash");
+	            CanDash = false;
+	            DashTimer = DashTime;
+	            DashReloadTimer = DashReloadTime;
+	            DashDir = direction.Normalized();
+	            Velocity = DashDir * DashSpeed;
+	        }
+	        else
+	        {
+	            Velocity = direction != Vector2.Zero
+	                ? direction.Normalized() * Speed
+	                : Vector2.Zero;
+	        }
+	    }
+
+	    MoveAndSlide();
+	}
+
+	public void Die()
+    {
+		if (IsDead)
+			return;
+		IsDead = true;
+    }
+
+	public void TakeDamage(int amount)
+	{
+		if(IsDead)
+			return;
+		if(!CanTakeDamage)
+			return;
+
+		CanTakeDamage = false;
+		CanTakeDamageTimer.Start();
+        Health -= amount;
+	}
+
+	public void _on_can_take_damage_timer_timeout()
+	{
+		CanTakeDamage = true;
 	}
 }
--- Scripts/Follower.cs
+++ Scripts/Follower.cs
@@ -6,54 +6,53 @@ using System;
 
 public partial class Follower : CharacterBody2D
 {
-	public const float speed = 200.0f;
-	
+	[Signal] public delegate void FollowerHitEventHandler();
+	[Export] private Area2D followerArea;
+	[Export] private Area2D followArea;
+	public const float speed = 80.0f;
 	private CharacterBody2D Player;
 	private bool Playerflag;
-	[Signal]
-	public delegate void FollowerHitEventHandler();
 	
-
-
-	public override void _Ready(){
-		Area2D followerArea = GetNode<Area2D>("FollowerHitArea");
-		Area2D followArea = GetNode<Area2D>("FollowArea");
+	public override void _Ready()
+	{
 		followerArea.AreaEntered += OnAreaEntered;
 		Player = GetParent().GetNode<CharacterBody2D>("Detonator");
 		followArea.BodyEntered += OnBodyEntered;
-		followArea.BodyExited += OnBodyExited;
-		
+		followArea.BodyExited += OnBodyExited;		
 	}
-
-
 
 	public override void _PhysicsProcess(double delta)
 	{
 	   Vector2 velocity = Vector2.Zero;
 	   
-	   
-		if (Playerflag && Player != null){
+		if (Playerflag && Player != null)
+		{
 			velocity = Position.DirectionTo(Player.Position) * speed;
 		}
-		else{
+		else
+		{
 			velocity = Vector2.Zero;
 		}
 		
-
 		Velocity = velocity;
 		MoveAndSlide();
 	}
-	private void OnAreaEntered(Area2D area){
+
+	private void OnAreaEntered(Area2D area)
+	{
 		
 	}
-	private void OnBodyEntered(Node2D body){
-		if(body.Name == "Detonator"){
+	private void OnBodyEntered(Node2D body)
+	{
+		if(body.Name == "Detonator")
+		{
 			Playerflag = true;
 		}
 	
 	}
 	
-	private void OnBodyExited(Node2D body){
+	private void OnBodyExited(Node2D body)
+	{
 		if(body.Name == "Detonator"){
 			Playerflag = false;
 		}

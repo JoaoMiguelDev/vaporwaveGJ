7bd7dd0d96e738ca8d786f30983828024d797ba7
Adonissys <adonisfilipe1@gmail.com>
2026-07-26 05:49:22 -0300

    Update
    Adição de musica no menu

--- Scenes/MainMenu/MainMenu.cs
+++ Scenes/MainMenu/MainMenu.cs
@@ -5,6 +5,7 @@ public partial class MainMenu : Godot.Control
 {
 	[Export] private Button start;
 	[Export] private AnimatedSprite2D art;
+	[Export] private AudioStreamPlayer2D music;
 	private PackedScene start_level = GD.Load<PackedScene>("res://Scenes/Levels/level_1.tscn");
 	
 	
@@ -14,6 +15,7 @@ public partial class MainMenu : Godot.Control
 		start.ButtonDown += OnStartButtonDown;
 		start.MouseEntered += OnMouseEntered;
 		start.MouseExited += OnMouseExited;
+		music.Play();
 		
 	}
 

c13f8cb4b2d5702e3780baa7f38a7368019890db
Adonissys <adonisfilipe1@gmail.com>
2026-07-26 04:45:36 -0300

    Menu Completo e efeitos sonoros

--- Scenes/MainMenu/MainMenu.cs
+++ Scenes/MainMenu/MainMenu.cs
@@ -4,7 +4,7 @@ using System;
 public partial class MainMenu : Godot.Control
 {
 	[Export] private Button start;
-	[Export] private Button exit;
+	[Export] private AnimatedSprite2D art;
 	private PackedScene start_level = GD.Load<PackedScene>("res://Scenes/Levels/level_1.tscn");
 	
 	
@@ -12,14 +12,20 @@ public partial class MainMenu : Godot.Control
 	public override void _Ready()
 	{
 		start.ButtonDown += OnStartButtonDown;
-		exit.ButtonDown += OnExitButtonDown;
+		start.MouseEntered += OnMouseEntered;
+		start.MouseExited += OnMouseExited;
+		
 	}
 
 	private void OnStartButtonDown(){
 		GetTree().ChangeSceneToPacked(start_level);
 	}
 	
-	private void OnExitButtonDown(){
-		GetTree().Quit();
+	private void OnMouseEntered(){
+		art.Animation = "Activated";
+	}
+
+	private void OnMouseExited(){
+		art.Animation = "Normal";
 	}
 }
